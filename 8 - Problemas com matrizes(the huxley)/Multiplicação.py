9ba5459644a714ea1d7aead241e8dba8d4c82cd0
aryelson Gonçalves Messias <78511762+aryelson1@users.noreply.github.com>
2021-04-30 12:20:58 -0300

    Questões

--- 8 - Problemas com matrizes(the huxley)/Multiplicação.py
+++ 8 - Problemas com matrizes(the huxley)/Multiplicação.py
@@ -22,3 +22,24 @@ while True:
         for j in i:
             print(j, end = ' ')
         print()
+       
+'''
+MODO REDUZIDO
+
+while True:
+    n = int(input())
+    if n == 0:
+        break
+    matriz = [ [" "]*4 for i in range(4) ]
+    for i in range(4):
+        linha = []
+        for j in range(4):
+            matriz[j][i]= int(input())
+    for i in range(4):
+        matriz[i][i] *= n
+    
+    for i in matriz:
+        for j in i:
+            print(j, end = ' ')
+        print()
+'''
